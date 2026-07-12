3d544b08dad91e6ec3fb2d1c1aab838718eac227
Eric Drejza <edrejza@udel.edu>
2026-07-12 13:10:06 -0400

    create deploy config for github.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/combat-zone/" : "/",
   plugins: [react()],
   test: {
     environment: "jsdom",
