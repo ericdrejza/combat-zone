19ba14dde2aa36faaf2f4f550807cfd7c3f0d90a
Eric Drejza <edrejza@udel.edu>
2026-07-18 16:09:47 -0400

    Fix upload bug by using path-browserify

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,28 @@
-import { fileURLToPath, URL } from "node:url";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vitest/config";
+import { fileURLToPath, URL } from 'node:url';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/combat-zone/" : "/",
+  base: process.env.NODE_ENV === 'production' ? '/combat-zone/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
-      "@core": fileURLToPath(new URL("./src/core", import.meta.url)),
-      "@entities": fileURLToPath(new URL("./src/entities", import.meta.url)),
-      "@hooks": fileURLToPath(new URL("./src/hooks", import.meta.url)),
-      "@interaction": fileURLToPath(new URL("./src/interaction", import.meta.url)),
-      "@library": fileURLToPath(new URL("./src/library", import.meta.url)),
-      "@store": fileURLToPath(new URL("./src/store", import.meta.url)),
-      "@tests": fileURLToPath(new URL("./tests", import.meta.url)),
-      "@ui": fileURLToPath(new URL("./src/ui", import.meta.url))
+      '@core': fileURLToPath(new URL('./src/core', import.meta.url)),
+      '@entities': fileURLToPath(new URL('./src/entities', import.meta.url)),
+      '@hooks': fileURLToPath(new URL('./src/hooks', import.meta.url)),
+      '@interaction': fileURLToPath(
+        new URL('./src/interaction', import.meta.url)
+      ),
+      '@library': fileURLToPath(new URL('./src/library', import.meta.url)),
+      '@store': fileURLToPath(new URL('./src/store', import.meta.url)),
+      '@tests': fileURLToPath(new URL('./tests', import.meta.url)),
+      '@ui': fileURLToPath(new URL('./src/ui', import.meta.url)),
+      path: 'path-browserify'
     }
   },
   test: {
-    environment: "jsdom",
+    environment: 'jsdom',
     globals: true,
-    setupFiles: "./vitest.setup.ts"
+    setupFiles: './vitest.setup.ts'
   }
 });
